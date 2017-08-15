7abda28d69ecc869b16313ca9a0cf902eafb5af5
Wisn <wuyishun_kmk@outlook.com>
2017-08-15 19:12:22 +0800

    keys

--- js/pages/PopularBar.js
+++ js/pages/PopularBar.js
@@ -18,9 +18,10 @@ import DataRepository, {Flag_storage} from '../expand/dao/DataRepository';
 import PopularItem from '../component/PopularItem';
 import PopularPage from './me/PopularPage';
 import Favorite from '../model/Favorite';
-
+import FavoriteDao from '../expand/dao/FavoriteDao';
 const URL = 'https://api.github.com/search/repositories?q=';
 const SortByKey = '&sort=starts';
+const  favoriteDao=new FavoriteDao();
 export default class PopularBar extends Component {
     constructor(props) {
         super(props);
@@ -29,11 +30,16 @@ export default class PopularBar extends Component {
             result: '',
             dataSource: new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2}),
             isRefresh: true,
+            keys:[],
         };
     }
 
     componentDidMount() {
         this.onLoad(false);
+    }
+    updateState(data){
+        if(!this) return ;
+        this.setState(data);
     }
     flushData(result){
         var AllFavorite=[];
@@ -46,8 +52,26 @@ export default class PopularBar extends Component {
         })
     }
     getIsFavorite(item){
-        //TODO判断是否是收藏
-        return true;
+        for(var i=0,len=this.state.keys.length;i<len;i++){
+            if(this.state.keys.toString()===item){
+                return true;
+            }
+        }
+        return false;
+    }
+    getFavoriteKeys(){
+        favoriteDao.getFavoriteKeys()
+            .then(result=>{
+                if(!result){
+                    this.updateState({
+                        keys:result,
+                    })
+                }
+                this.flushData(result);
+            })
+            .catch(error=>{
+                this.flushData(error);
+            })
     }
     onLoad(isRefresh) {
         this.setState({
@@ -56,13 +80,13 @@ export default class PopularBar extends Component {
         var url = this.getUrl();
         this.DataRepository.fetchResponsitory(url, isRefresh)
             .then(result => {
-                this.flushData(result);
+                this.getFavoriteKeys();
                 this.setState({
                     isRefresh: false,
                 })
             })
             .catch(error => {
-               this.flushData(error);
+                this.getFavoriteKeys();
                 this.setState({
                     isRefresh: false,
                 })
